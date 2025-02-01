220e900b7175b6350007603c7037698d236f21f2
“richardcrn3rd” <richardcrn3rd@gmail.com>
2025-02-01 12:15:43 -0500

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import App from './App'
 // import renderer from 'react-test-renderer'
 
 // This first test is commented out since we have the other snapshot test that checks the GitHubCard component
-// test('renders a snapshot', () => {
-//   const tree = renderer.create(<App/>).toJSON()
-//   expect(tree).toMatchSnapshot()
-// })
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 test('renders text MickeyMouse', () => {
   render(<App />)
